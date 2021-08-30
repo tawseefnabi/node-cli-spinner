619980347aaa9d0436bdc347a8fe198a7ac1bd64
tawseef <tawseefnabi9@gmail.com>
2021-08-30 20:11:17 +0530

    fix for npm

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
+const spinners = require('./spinners.json');
+import spinners from './spinners'
 
-// const spinners = require('./spinners.json');
-// import spinners from './spinners'
-
-// (async () => {
-//   console.log('here');
-//   const randomIndex = Math.floor(Math.random() * Object.keys(spinners).length);
-//   const spinner = Object.keys(spinners)[randomIndex];
-//   console.log('spinner', spinners[spinner]);
-//   return spinners[spinner]
-// })()
-
-// 'use strict';
-
-const spinners = Object.assign({}, require('./spinners.json'));
-
-const spinnersList = Object.keys(spinners);
-
-spinners = Object.defineProperty(spinners, 'random', {
-	get() {
-		const randomIndex = Math.floor(Math.random() * spinnersList.length);
-		const spinnerName = spinnersList[randomIndex];
-		return spinners[spinnerName];
-	}
-});
-
-module.exports = spinners;
+module.exports = () => {
+  console.log('here');
+  const randomIndex = Math.floor(Math.random() * Object.keys(spinners).length);
+  const spinner = Object.keys(spinners)[randomIndex];
+  console.log('spinner', spinners[spinner]);
+  return spinners[spinner]
+}
--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-import test from 'ava';
-import cliSpinners from require('.');
+import test from "ava";
+import cliSpinners from ".";
 
+console.log("Spinner count:", Object.keys(cliSpinners).length);
 
-console.log('Spinner count:', Object.keys(spinners).length);
-
-test('main', t => {
-  console.log('test starts');
-	t.is(typeof cliSpinners, 'object');
-	t.is(cliSpinners.dots.interval, 80);
-	t.true(Array.isArray(cliSpinners.dots.frames));
+test("main", (t) => {
+  console.log("test starts");
+  t.is(typeof cliSpinners, "object");
+  t.is(cliSpinners.dots.interval, 80);
+  t.true(Array.isArray(cliSpinners.dots.frames));
 });
-
