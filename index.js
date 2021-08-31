b914ecc10c395cf3ef11387765f274d896c80542
tawseef <tawseefnabi9@gmail.com>
2021-08-31 20:13:06 +0530

    minor change

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const spinners = require('./spinners.json');
-import spinners from './spinners'
+const spinners = require("./spinners.json");
+import spinners from "./spinners";
 
 module.exports = () => {
-  console.log('here');
+  console.log("here");
   const randomIndex = Math.floor(Math.random() * Object.keys(spinners).length);
   const spinner = Object.keys(spinners)[randomIndex];
-  console.log('spinner', spinners[spinner]);
-  return spinners[spinner]
-}
+  console.log("spinner", spinners[spinner]);
+  return spinners[spinner];
+};
